b140b0f539d7891a0189184629b60bb7e65a678d
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-07 01:37:45 +0530

    Lecture 7

---
+++ Lectures/Lecture7_JavaMethods/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    //main is a special method in java that marks the entry point of java program.
+    //a method cannot be inside another method but it needs to be inside a class.
+    //method structure: public static method_return_datatype methodName(parameters) {method body}
+    //method can be called by methodName(parameters); as a statement.
+    //return statement returns control to the invoker of the method. it is not needed when the return datatype of the method is void. ie the return value can now be used where it is called (stored in a variable/used in an expression)
+    //A method with a void return type ( a method that does not return anything ) is called a procedure.
+    //If a return statement is not specified in a method with non-void return type, then it will throw a compile time error. If the return statement is in an if else block then we must make sure that all if else conditions have a return type.
+  }
+}
