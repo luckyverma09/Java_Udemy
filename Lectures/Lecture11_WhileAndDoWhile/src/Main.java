81da09f0bb6f5e8514de71a9a17eaeaf3d62e825
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-07 01:38:40 +0530

    Lecture 11

---
+++ Lectures/Lecture11_WhileAndDoWhile/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    int i = 10;
+    while(i > 5){
+      System.out.println(i);
+      i--;
+    }
+    i = 10;
+    do{
+      System.out.println(i);
+      i--;
+    }while(i > 5);
+  }
+}
