2e5668896aaa78843541af4f7450535a99d0d807
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-07 22:30:06 +0530

    Challenge 4

---
+++ Challenges/Challenge4_OperatorChallenge/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+        int result = 10;
+        result -= 5;
+        System.out.println("result : " + result);
+    }
+}
