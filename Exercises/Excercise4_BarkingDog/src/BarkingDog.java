b99f77918eec57ead95de3d8ddfdef5a33127b88
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-08 16:57:45 +0530

    exercise 4

---
+++ Exercises/Excercise4_BarkingDog/src/BarkingDog.java
@@ -0,0 +1,11 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if(hourOfDay < 0 || hourOfDay > 23) return false;
+
+        if((hourOfDay < 8 || hourOfDay > 22) && barking){
+            return true;
+        }
+
+        return false;
+    }
+}
