e166f57a6a065cb391b11e981b090c338f476f86
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-08 13:33:49 +0530

    Challenge 7

--- Challenges/Challenge7_methodChallenge/src/Main.java
+++ Challenges/Challenge7_methodChallenge/src/Main.java
@@ -1,9 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-    
+    int highScorePosition1 = calculateHighScorePosition(1500);
+    displayHightScorePosition("Lucky", highScorePosition1);
+
+        int highScorePosition2 = calculateHighScorePosition(500);
+        displayHightScorePosition("Piyush", highScorePosition2);
+
+        int highScorePosition3 = calculateHighScorePosition(100);
+        displayHightScorePosition("Vanshika", highScorePosition3);
+
+        int highScorePosition4 = calculateHighScorePosition(25);
+        displayHightScorePosition("Teju", highScorePosition4);
     }
     public static void displayHightScorePosition(String playerName, int playerPosition){
-        System.out.println(playerName + " maneged to get into position " + playerPosition +"on the high score list");
+        System.out.println(playerName + " maneged to get into position " + playerPosition +" on the high score list.");
     }
     public static int calculateHighScorePosition(int playerScore){
         if(playerScore >= 1000) return 1;
