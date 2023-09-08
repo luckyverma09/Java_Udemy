00da9338b637cda3786f01b574880b1c9c0e4952
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-08 15:48:38 +0530

    exercise 2

---
+++ Exercises/Excercise2_SpeedConverter/src/SpeedConverter.java
@@ -0,0 +1,20 @@
+public class SpeedConverter {
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            double milesPerHour = kilometersPerHour / 1.609; // 1 mile = 1.60934 kilometers
+            return Math.round(milesPerHour);
+        }
+    }
+}
