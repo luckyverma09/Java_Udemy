217a75dad2d422d9c6fe88d529a2fb6ebff13f4c
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-08 21:46:32 +0530

    exercise 8

---
+++ Exercises/_8_TeenNumberChecker/src/TeenNumberChecker.java
@@ -0,0 +1,9 @@
+public class TeenNumberChecker {
+    public static boolean hasTeen(int a, int b, int c) {
+        return isTeen(a) || isTeen(b) || isTeen(c);
+    }
+
+    public static boolean isTeen(int age) {
+        return age >= 13 && age <= 19;
+    }
+}
