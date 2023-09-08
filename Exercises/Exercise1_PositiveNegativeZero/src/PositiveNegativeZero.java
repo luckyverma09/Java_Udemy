d808b24dbfe269f946cb54df41a20e6b634a1356
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-08 15:19:44 +0530

    exercise 1

---
+++ Exercises/Exercise1_PositiveNegativeZero/src/PositiveNegativeZero.java
@@ -0,0 +1,7 @@
+public class PositiveNegativeZero {
+    public static void checkNumber(int num){
+        if(num > 0) System.out.println("positive");
+        if(num == 0) System.out.println("zero");
+        if(num < 0) System.out.println("negative");
+    }
+}
