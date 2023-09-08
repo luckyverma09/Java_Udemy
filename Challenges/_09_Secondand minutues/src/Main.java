81fd654bbe440dbed298e79ab51d82f636561b4b
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-08 23:46:10 +0530

    Challenge 9

---
+++ Challenges/_09_Secondand minutues/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(65,45));
+    }
+
+    public static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            return "Invalid Value";
+        }
+
+        int minutes = seconds / 60;
+        return getDurationString(minutes,seconds);
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+
+        seconds %= 60;
+        int hours = minutes / 60;
+        minutes %= 60;
+
+        return (hours + "h " + minutes + "m " + seconds + "s");
+    }
+}
