08293cdfe33db20fa535f38f602577d5fec2f5ae
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-10 16:53:12 +0530

    Update PerfectNumber.java

--- Exercises/_25_PerfectNumber/src/PerfectNumber.java
+++ Exercises/_25_PerfectNumber/src/PerfectNumber.java
@@ -1,9 +1,9 @@
 public class PerfectNumber {
     public static boolean isPerfectNumber(int number){
-        if(number <0) return false;
+        if(number <=0) return false;
         int sum = 0;
 
-        for(int i =1; i< number; i++){
+        for(int i =1; i<= number/2; i++){
             if(number % i == 0){
                 sum +=i;
             }
