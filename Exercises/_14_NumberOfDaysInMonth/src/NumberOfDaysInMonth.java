2a5308be6549614920ef349e08a3316197ebe6d4
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-10 12:50:53 +0530

    exercise 13

---
+++ Exercises/_14_NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
@@ -0,0 +1,35 @@
+public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year) {
+        if (year <= 0 || year > 9999) {
+            return false;
+        }
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 0 || month > 12) return -1;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            case 2: {
+                if (isLeapYear(year)) {
+                    return 29;
+                } else return 28;
+            }
+        }
+        return -1;
+    }
+
+
+}
