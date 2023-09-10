ff8aa2d552f98484dfd73cb5ccc521b0f7e34396
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-10 14:28:24 +0530

    challenge 15

--- Challenges/_15_sumDigits/src/Main.java
+++ Challenges/_15_sumDigits/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(sumDigit(1000));
+    }
+
+    public static int sumDigit(int number) {
+        if (number < 0) return -1;
+        if (number <= 9) return number;
+        int sum = 0;
+
+        while (number != 0) {
+            int rem = number % 10;
+            sum += rem;
+            number /= 10;
+        }
+        return sum;
     }
 }
