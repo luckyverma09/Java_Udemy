64abf031aaf004fa4e2e6b6501da7f6d94f24438
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-11 10:34:14 +0530

    exercise 31

---
+++ Exercises/_31_PaintJob/src/PaintJob.java
@@ -0,0 +1,26 @@
+public class PaintJob {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if(width <= 0 || height <= 0 || areaPerBucket <=0 || extraBuckets < 0) {
+            return -1;
+        }else{
+            double area = width*height;
+            double bucketRequired = Math.ceil(area / areaPerBucket);
+            return (int)bucketRequired - extraBuckets;
+        }
+    }
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+        if(width <= 0 || height <= 0 || areaPerBucket <=0 ) {
+            return -1;
+        }else{
+            double area = width*height;
+            return  (int)Math.ceil(area / areaPerBucket);
+        }
+    }
+    public static int getBucketCount(double area, double areaPerBucket){
+        if(area <= 0 || areaPerBucket <=0 ) {
+            return -1;
+        }else{
+            return  (int)Math.ceil(area / areaPerBucket);
+        }
+    }
+}
