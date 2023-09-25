c5e100d14ed50efb7d6e755f403811cc4735636a
Lucky Verma <idonthaveemail1af@gmail.com>
2023-09-25 10:20:52 +0530

    commit

---
+++ Exercises/_35_Point/src/Point.java
@@ -0,0 +1,41 @@
+public class Point {
+    private int x ;
+    private int y;
+
+    public Point(){
+        this(0,0);
+    }
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt(x*x + y*y);
+    }
+    public double distance(int x,int y){
+       int xDiff = this.x - x;
+       int yDiff = this.y - y;
+       return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+    public double distance(Point another){
+        int xDiff = this.x - another.getX();
+        int yDiff = this.y - another.getY();
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+}
