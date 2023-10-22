b8bc1d91e6384eb98b1b556b7d83e6cf841b493e
Lucky Verma <117267237+luckyverma09@users.noreply.github.com>
2023-10-22 23:05:53 +0530

    commit

--- Challenges/_27_ReverseArray/src/Main.java
+++ Challenges/_27_ReverseArray/src/Main.java
@@ -4,9 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {1,2,3,4,5,6,7,8,9};
         reverse(array);
-        for(int i = 0 ;i< array.length; i++){
-            System.out.print(array[i] + " ");
-        }
+        System.out.println(Arrays.toString(array));
 
     }
     public static void reverse(int[] arr){
---
+++ Challenges/_45_ReverseArray/src/ReverseArray.java
@@ -0,0 +1,15 @@
+public class ReverseArray {
+    private static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+}
